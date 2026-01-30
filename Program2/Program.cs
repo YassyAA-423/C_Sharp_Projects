8f0d7d4080d88a4412e4582dbed1b10e4958557b
YassyAA-423 <yaaguilera42@gmail.com>
2026-01-29 21:19:02 -0700

    Add project files.

---
+++ Program2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    internal class Techademydailyreport
+    {
+    
+        static void Main()
+
+
+        {
+            Console.WriteLine("Techademy Daily Report");
+            Console.ReadLine();
+
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+
+            //This is a simple program that asks for your name
+            Console.WriteLine("What is your name?");
+            string yourname = Console.ReadLine();
+            Console.WriteLine("Your name is: " + yourname);
+            Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string yourcourse = Console.ReadLine();
+            Console.WriteLine("Your Course: " + yourcourse);
+            Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            int pagenumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are on page number: " + pagenumber);
+            Console.ReadLine();
+
+            Console.WriteLine("Do you need help with anything? Please answer with \"true\" or \"false\".");
+            bool needhelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Need help: " + needhelp);
+            Console.ReadLine();
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveexp = Console.ReadLine();
+            Console.WriteLine("Positive experiences: " + positiveexp);
+            Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("Feedback: " + feedback);
+            Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            int hourstoday = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours studied today: " + hourstoday);
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+
+
+            //Different Data Types
+            //Boolean example
+            //bool isStudying = false;
+
+            ////Byte example
+            //byte hoursworked = 42;
+
+            ////sbyte example
+            //sbyte currenttempurature = -23;
+
+            ////char example
+            //char questionmark = '\u2103';
+
+            ////decimal example
+            //decimal moneyinbank = 100.5m;
+
+
+            ////double example
+            //double heightincentimeters = 211.456789012345;
+
+            ////float example
+            //float secondsLeft = 2.62f;
+
+            ////short example
+            //short tempuratureonMars = -341;
+
+            ////String example
+            //String myName = "Brandon";
+
+
+            ////Int to String
+            //int currentage = 27;
+            //string yearsold = currentage.ToString();
+
+            ////Bool to String
+            //bool isRaining = true;
+            //string raining = Convert.ToString(isRaining);
+
+
+
+
+
+            //Console.WriteLine(isRaining);
+            //Console.ReadLine();
+        }
+    }
+}
+ 
---
+++ Program2/TechAcademyReport.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//This is a simple console progam that collects daily report information from a student
+namespace TechAcademyReport
+{
+    internal class DailyReport
+    {
+        static void Main()
+        {
+            Console.WriteLine("Techademy Daily Report");
+            Console.ReadLine();
+
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+
+            //This is a program/question asks for your name
+            Console.WriteLine("What is your name?");
+            string yourname = Console.ReadLine();
+            Console.WriteLine("Your name is: " + yourname);
+            Console.ReadLine();
+
+
+            //This is a program/question asks what course you are on
+            Console.WriteLine("What course are you on?");
+            string yourcourse = Console.ReadLine();
+            Console.WriteLine("Your Course: " + yourcourse);
+            Console.ReadLine();
+
+            //This is a program/question asks for the page number
+            Console.WriteLine("What page number?");
+            int pagenumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are on page number: " + pagenumber);
+            Console.ReadLine();
+
+            //This is a program/question asks if you need help
+            Console.WriteLine("Do you need help with anything? Please answer with \"true\" or \"false\".");
+            bool needhelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Need help: " + needhelp);
+            Console.ReadLine();
+
+            //This is a program/question asks if you had any positive experiences
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveexp = Console.ReadLine();
+            Console.WriteLine("Positive experiences: " + positiveexp);
+            Console.ReadLine();
+
+            //This is a program/question asks if you had any other feedback
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("Feedback: " + feedback);
+            Console.ReadLine();
+
+            //This is a program/question asks how many hours you studied 
+            Console.WriteLine("How many hours did you study today?");
+            int hourstoday = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours studied today: " + hourstoday);
+            Console.ReadLine();
+
+            //This is a program simply thanks you for your answers
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
+ 
