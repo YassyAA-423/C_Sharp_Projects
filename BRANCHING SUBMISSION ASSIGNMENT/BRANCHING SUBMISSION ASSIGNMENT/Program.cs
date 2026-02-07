b8aa77b1b6ee3252843cbc702d3de66979892397
YassyAA-423 <yaaguilera42@gmail.com>
2026-02-06 18:29:47 -0700

    Branching Submission
    This is my submission for the Branching assignment.

---
+++ BRANCHING SUBMISSION ASSIGNMENT/BRANCHING SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRANCHING_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                return;
+            }
+
+            Console.WriteLine("Please enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            if (height > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                return;
+            }
+
+            Console.WriteLine("Please enter the package length:");
+           int length = Convert.ToInt32(Console.ReadLine());
+            if (length > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                return;
+            }
+            Console.WriteLine("Please enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+            if (width > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                return;
+            }
+            int total = (height * length * width) * weight / 100;
+            Console.WriteLine("Your estimated total for shipping this package is: $" + total);
+            Console.WriteLine("Thank you!");
+
+        }
+    }
+}
